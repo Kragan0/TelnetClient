c52994822d9fd03795d88729397131cece58126b
Kragan0 <alex.geleta11@gmail.com>
2024-10-04 15:36:40 +0200

    First Commit

---
+++ app/src/main/java/utb/fai/App.java
@@ -0,0 +1,13 @@
+package utb.fai;
+
+public class App {
+
+    public static void main(String[] args) {
+        // TODO: Implement input parameter processing
+        // String server_ip = args[0];
+        // int port = Integer.parseInt(args[1]);
+        
+        TelnetClient telnetClient = new TelnetClient("127.0.0.1", 23);
+        telnetClient.run(); // run telnet client
+    }
+}
---
+++ app/src/main/java/utb/fai/TelnetClient.java
@@ -0,0 +1,28 @@
+package utb.fai;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class TelnetClient {
+
+    private String serverIp;
+    private int port;
+
+    public TelnetClient(String serverIp, int port) {
+        this.serverIp = serverIp;
+        this.port = port;
+    }
+    
+    public void run() {
+        try {
+            Socket socket = new Socket(serverIp, port);
+            // Implementation of receiving and sending data
+            
+
+            // Implement processing of input from the user and sending data to the server
+            // Implement response processing from the server and output to the console
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
